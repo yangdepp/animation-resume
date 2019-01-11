d373d65e151e0e95eeeb98ec9afc02ab63f78f42
yangdepp <965093975@qq.com>
2019-01-11 16:58:45 +0800

    继承

--- js/test2.js
+++ js/test2.js
@@ -20,24 +20,76 @@
 //JSON 和 JavaScript 的区别有哪些？
 
 
-function likeClass(a, b) {
-  this.a = a;
-  this.b = b;
-  this.getA = function(){
-    return this.a + 1;
-  }
-  this.getB = function(){
-    return this.b + 1;
-  }
+// function likeClass(a, b) {
+//   this.a = a;
+//   this.b = b;
+//   this.getA = function(){
+//     return this.a + 1;
+//   }
+//   this.getB = function(){
+//     return this.b + 1;
+//   }
+// }
+
+// likeClass.prototype.say = function () {
+//   console.log('hello,world.')
+// }
+
+// var t = new likeClass(1,2);
+// console.log(t.a);
+// console.log(t.b)
+// t.getA();
+// t.getB();
+
+
+// function fn() {
+// }
+// fn.prototype.color = 'red'
+
+// var f = new fn();
+
+// // 实例f的原型，指向构造函数
+// console.log(f.__proto__ === fn.prototype)
+// console.log(fn.prototype.__proto__ === Object.prototype)
+// console.log(Object.prototype.__proto__ === null)
+// console.log(Object.__proto__ === Function.prototype)
+
+// console.log(f.__proto__.constructor === fn)
+// console.log(fn.prototype.constructor === fn)
+// console.log(f.constructor === fn)
+
+// 实现继承
+function Shape() {
+  this.x = 1;
+  this.y = 1;
+}
+Shape.prototype.move = function (x, y) {
+  this.x += x;
+  this.y += y;
+  console.log('Shape moved.');
+  console.log(this.x)
+  console.log(this.y)
+}
+Shape.prototype.move2 = function () {
+  console.log('move2')
 }
 
-likeClass.prototype.say = function () {
-  console.log('hello,world.')
+function Rectangle() {
+  Shape.call(this);
 }
+// 这么写，子类会整体继承父类，
+// Object.create 接受一个对象作为参数，然后以它为原型，返回一个实例对象，改实例对象完全继承原型对象的属性
 
-var t = new likeClass(1,2);
-console.log(t.a);
-console.log(t.b)
-t.getA();
-t.getB();
+Rectangle.prototype = Object.create(Shape.prototype)
+Rectangle.prototype.constructor = Rectangle
 
+// 如果需要单个方法继承，可以用
+// Rectangle.prototype.move2 = function(){
+//   Shape.prototype.move2.call(this)
+// }
+
+var rect = new Rectangle()
+rect.move(4,5)
+rect.move2()
+
+console.log(rect instanceof Shape)
